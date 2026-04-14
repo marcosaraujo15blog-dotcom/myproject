36e46129ac8da168e20f607fc728864171bfba59
marcosaraujo15blog <marcosaraujo15blog@gmail.com>
2026-04-14 17:57:06 -0300

    Versão 1.01

---
+++ motoristas/migrations/0003_timestamps.py
@@ -0,0 +1,32 @@
+import django.utils.timezone
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('motoristas', '0002_indexes'),
+    ]
+
+    operations = [
+        migrations.AddField(
+            model_name='motorista',
+            name='created_at',
+            field=models.DateTimeField(
+                auto_now_add=True,
+                default=django.utils.timezone.now,
+                verbose_name='Criado em',
+            ),
+            preserve_default=False,
+        ),
+        migrations.AddField(
+            model_name='motorista',
+            name='updated_at',
+            field=models.DateTimeField(
+                auto_now=True,
+                default=django.utils.timezone.now,
+                verbose_name='Atualizado em',
+            ),
+            preserve_default=False,
+        ),
+    ]
